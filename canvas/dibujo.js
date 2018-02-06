c636f83a05f506abdd8ffbd41b1bc41a94db8c1a
acramos86 <acramos86@hotmail.com>
2018-02-06 10:13:22 -0500

    Actualizando el dibujo antes de probar eventos con teclado

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -16,15 +16,29 @@ function trazarLinea(color, xinicial, yinicial, xfinal, yfinal){
 	trazarLinea("blue", i, 0, 300, (i+10));
 }*/
 
-
-while(l<=300){
+/*while(l<=300){
 	trazarLinea("black", 0, l, (l+10), 300);
-	trazarLinea("black", l, 0, 300, (l+10));
+	//trazarLinea("black", l, 0, 300, (l+10));
 	l = l + 10;
+}*/
+
+var dato=document.getElementById("texto_lineas");
+var dato_boton=document.getElementById("boton");
+
+function dibujoPorClick(){
+	var x = parseInt(dato.value);
+	for (var i = 0; i < 300; i = i + x ) {
+		trazarLinea("blue", 0, i, (i+10), 300);
+		trazarLinea("blue", i, 0, 300, (i+10));		
+	}
+	alert("Listo mijo");	
 }
 
-trazarLinea("black", 0, 0, 0, 300);
-trazarLinea("black", 0, 300, 300, 300);
-trazarLinea("black", 0, 0, 300, 0);
+dato_boton.addEventListener("click", dibujoPorClick );
 
+document.addEventListener("keydown", dibujoTeclado );
+
+function dibujoTeclado(){
+	alert("AAAHHHHHHHHHHH!!!!!");
+}
 
